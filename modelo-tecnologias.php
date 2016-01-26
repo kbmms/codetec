17b7e90693bb0c2ef92ddadf26d083ae8678e83e
kbmms <roxsublime@gmail.com>
2016-01-26 00:06:41 -0300

    adaptando a page com subpages e um modelo adicionado para as subpages single

---
+++ modelo-tecnologias.php
@@ -0,0 +1,23 @@
+<?php /* Template Name: subpages  */ ?>
+<?php get_header(); ?> 
+
+
+<?php the_post(); ?>
+
+
+<div class="container-fluid">
+  <div class="container single">
+     <h2><?php the_title(); ?></h2>
+    <p><?php the_field( "subtitulo" ); ?></p>
+    <p><a href="javascript:window.history.go(-1)">Voltar</a></p>
+    <?php if ( has_post_thumbnail() ) : ?>
+    <figure>
+      <?php the_post_thumbnail(); ?>
+    </figure>
+    <?php endif; ?>
+    
+    <?php the_content(); ?>
+     <?php comments_template(); ?>
+  </div>
+</div>
+<?php get_footer(); ?> 
--- page.php
+++ page.php
@@ -2,10 +2,35 @@
 
 <div class="container-fluid">
   <div class="container">
-    <h1>Página Page</h1>
-    <p><a href="#">Voltar para página inícial</a></p>
-  </div>
-</div>
+   <?php
+    global $post;
+    $args = array(
+      'post_parent'=> $post->ID,
+      'order' => 'ASC',
+      'post_type' => 'page',
+      'post_status' => 'publish',
+      'post_per_page' => -1
+    );
 
+    query_posts($args);
+      if(have_posts()) : while (have_posts()) : the_post();
+?>
+     <a href="<?php the_permalink(); ?>">
+    <div class="col-md-3 col-sm-6 col-xs-12">
+    <p><?php the_title(); ?></p>
+    <img class="img-responsive" src="<?php the_field( "imagem-subpage" ); ?>" alt="">
+    </div>
+    </a>
+    
+    
+    
+
+  <?php
+    endwhile;
+    endif;
+    wp_reset_query();
+  ?>
+    </div>
+</div>
 
 <?php get_footer(); ?> 
